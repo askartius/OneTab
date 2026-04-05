5960b9faf6631f1f4642391662bbbf865bdcc5ec
Askartius <151680098+askartius@users.noreply.github.com>
2026-04-06 02:09:11 +0800

    Initial commit

---
+++ app/src/main/java/askartius/onetab/MainActivity.java
@@ -0,0 +1,151 @@
+package askartius.onetab;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.FrameLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.progressindicator.CircularProgressIndicator;
+import com.google.android.material.search.SearchView;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView = findViewById(R.id.web_view);
+        FrameLayout actionButtonLayout = findViewById(R.id.action_button_layout);
+        CircularProgressIndicator progressIndicator = findViewById(R.id.progress_indicator);
+        MaterialButton actionButton = findViewById(R.id.action_button);
+        SearchView searchView = findViewById(R.id.search_view);
+
+        // Setup WebView
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setUserAgentString("Mozilla/5.0");
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.getSettings().setSaveFormData(false);
+        webView.getSettings().setGeolocationEnabled(false);
+        webView.getSettings().setDomStorageEnabled(false);
+        webView.getSettings().setDatabaseEnabled(false);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                progressIndicator.setProgress(newProgress);
+
+                if (newProgress >= 100) {
+                    progressIndicator.animate().alpha(0).setDuration(750);
+                } else if (progressIndicator.getAlpha() == 0) {
+                    progressIndicator.animate().alpha(1).setDuration(250);
+                }
+            }
+        });
+        webView.setOnScrollChangeListener((v, scrollX, scrollY, oldScrollX, oldScrollY) -> {
+            if (scrollY < oldScrollY && actionButtonLayout.getScaleX() == 0) {
+                actionButtonLayout.animate().scaleX(1).setDuration(50);
+                actionButtonLayout.animate().scaleY(1).setDuration(50);
+            }
+            if (scrollY > oldScrollY && actionButtonLayout.getScaleX() == 1) {
+                actionButtonLayout.animate().scaleX(0).setDuration(50);
+                actionButtonLayout.animate().scaleY(0).setDuration(50);
+            }
+        });
+        //webView.clearCache(true);
+
+        // Disable cookies
+        CookieManager.getInstance().setAcceptThirdPartyCookies(webView, false);
+
+        // Setup back gesture handling
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (searchView.isShowing()) {
+                    searchView.hide();
+                    return;
+                }
+
+                if (webView.canGoBack()) {
+                    webView.stopLoading();
+                    webView.goBack();
+                } else {
+                    finish();
+                }
+            }
+        });
+
+        // Setup query button
+        actionButton.setOnClickListener(v -> {
+            searchView.setText(webView.getUrl());
+            searchView.getEditText().selectAll();
+            searchView.show();
+        });
+        actionButton.setOnLongClickListener(v -> {
+            webView.reload();
+            return true;
+        });
+
+        // Setup search
+        searchView.getEditText().setOnEditorActionListener((v, actionId, event) -> {
+            if (Patterns.WEB_URL.matcher(searchView.getText()).matches()) {
+                webView.loadUrl(searchView.getText().toString());
+            } else {
+                webView.loadUrl("https://www.bing.com/search?q=" + searchView.getText());
+            }
+
+            searchView.hide();
+
+            return true;
+        });
+
+        handleIntent(getIntent());
+    }
+
+    @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+
+        setIntent(intent);
+        handleIntent(intent);
+    }
+
+    private void handleIntent(@NonNull Intent intent) {
+        if (Objects.equals(intent.getAction(), Intent.ACTION_VIEW)) {
+            Uri uri = intent.getData();
+
+            if (uri != null && (Objects.equals(uri.getScheme(), "https") || Objects.equals(uri.getScheme(), "http"))) {
+                webView.loadUrl(uri.toString());
+                return;
+            }
+        }
+
+        webView.loadUrl("https://www.bing.com");
+    }
+}
